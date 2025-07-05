ea69211167c982556fc862dccb56b9e8fb8f3e1e
Erik Peterson <erik@thepetecollective.com>
2025-07-05 10:22:43 -0500

    Initial networking with refresh logic commented

---
+++ Fay/Services/TokenStorage.swift
@@ -0,0 +1,103 @@
+//
+
+import Foundation
+
+struct Token: Codable {
+    let access: String
+    let refresh: String
+    let accessExpires: Date?
+    let refreshExpires: Date?
+    func accessExpired() -> Bool {
+        guard let accessExpires = accessExpires, accessExpires > Date.now else {
+            return true
+        }
+        return false
+    }
+    func refreshExpired() -> Bool {
+        guard let refreshExpires = refreshExpires, refreshExpires > Date.now else {
+            return true
+        }
+        return false
+    }
+}
+
+protocol TokenStorage {
+    func removeTokenFromStorage() async throws
+    func saveTokenToStorage(_ token: Token) async throws
+    func getTokenFromStorage() async throws -> Token
+}
+
+class KeychainTokenStorage: TokenStorage {
+    func getTokenFromStorage() async throws -> Token {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: "com.worshipworkbench.token-service",
+            kSecAttrAccount as String: "wwJsonWebToken",
+            kSecReturnAttributes as String: true,
+            kSecReturnData as String: true
+        ]
+        var item: CFTypeRef?
+        let result = SecItemCopyMatching(query as CFDictionary, &item)
+        
+        guard result == errSecSuccess else {
+            throw FayError.keychainRetrieveError
+        }
+        
+        guard
+            let keychainItem = item as? [String : Any],
+            let keychainData = keychainItem[kSecValueData as String] as? Data
+        else {
+            throw FayError.keychainRetrieveError
+        }
+        
+        let tokens = try JSONDecoder().decode(Token.self, from: keychainData)
+        return tokens
+    }
+    
+    func saveTokenToStorage(_ token: Token) async throws {
+        let data = try JSONEncoder().encode(token)
+        
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: "com.worshipworkbench.token-service",
+            kSecAttrAccount as String: "wwJsonWebToken",
+            kSecValueData as String: data,
+            kSecAttrAccessible as String: kSecAttrAccessibleWhenUnlocked
+        ]
+        let queryCF = query as CFDictionary
+        let result = SecItemAdd(queryCF, nil)
+        
+        if result == errSecDuplicateItem {
+            let updateQuery: [String: Any] = [
+                kSecClass as String: kSecClassGenericPassword,
+                kSecAttrService as String: "com.worshipworkbench.token-service",
+                kSecAttrAccount as String: "wwJsonWebToken",
+                kSecAttrAccessible as String: kSecAttrAccessibleWhenUnlocked
+            ]
+            let updateAttr = [
+                kSecValueData: data
+            ]
+            let updateResult = SecItemUpdate(updateQuery as CFDictionary, updateAttr as CFDictionary)
+            guard updateResult == errSecSuccess else {
+                throw FayError.keychainUpdate
+            }
+        }
+        else if result != errSecSuccess {
+            throw FayError.keychainSaveError
+        }
+    }
+    
+    func removeTokenFromStorage() async throws {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: "com.worshipworkbench.token-service",
+            kSecAttrAccount as String: "wwJsonWebToken"
+        ]
+        
+        let result = SecItemDelete(query as CFDictionary)
+        if result != errSecSuccess {
+            throw FayError.keychainDelete
+        }
+
+    }
+}
